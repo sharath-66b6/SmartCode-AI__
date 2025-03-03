c95b7a4ad6624eba0bc8ad3fe19a8df9d81b546c
Sharath kumar Reddy <sharathkumarr924@gmail.com>
2025-03-03 18:06:00 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,20 @@ require('dotenv').config()
 const app = require('./src/app') 
 
 
+const allowedOrigins = [
+    "https://smart-code-ai.vercel.app", // Frontend URL
+    "https://smartcode-ai.vercel.app", // Backend URL
+    "http://localhost:5173", // Keep for local testing
+  ];
+  
+  app.use(
+    cors({
+      origin: "*", // TEMPORARY: Allows all origins (not recommended for production)
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,19 +5,6 @@ const app = express()
 
 const cors = require("cors");
 
-const allowedOrigins = [
-    "https://smart-code-ai.vercel.app", // Frontend URL
-    "https://smartcode-ai.vercel.app", // Backend URL
-    "http://localhost:5173", // Keep for local testing
-  ];
-  
-  app.use(
-    cors({
-      origin: "*", // TEMPORARY: Allows all origins (not recommended for production)
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
 
 app.use(express.json())
 
